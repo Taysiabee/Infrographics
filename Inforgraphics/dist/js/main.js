aa685c6bcbf9addc78ada058b2e13bc9b6c984c9
Taysiabee <50152275+Taysiabee@users.noreply.github.com>
2019-11-07 13:40:39 -0500

    Add files via upload

---
+++ Inforgraphics/dist/js/main.js
@@ -0,0 +1,2 @@
+"use strict";var disappearAnimation=TweenMax.from(".magician",1.5,{opacity:0,delay:3,repeat:50}),bounceAnimaion=TweenMax.to(".ball",2.5,{ease:Bounce.easeOut,y:-50,repeat:50}),coilAnimation=TweenLite.to(".coil",10,{rotation:810,ease:Power3.easeInOut}),puffAnimation=TweenLite.to(".puff",10,{rotation:810,ease:Power3.easeInOut});
+//# sourceMappingURL=main.js.map
---
+++ Inforgraphics/src/js/main.js
@@ -0,0 +1,42 @@
+// const bounceAnimation = new TimelineMax();
+
+// bounceAnimation.fromTo(".ball", 1, {
+//   x:"+=400"
+// }, {
+//   y:"-=400",
+//   yoyo:true,
+//   repeat:10
+// })
+// .repeatDelay(.5)
+// .repeat(-1);
+
+// const disappearAnimation =TweenMax.from(".magician", 1.5, {
+
+// 	opacity:0, 
+// 	delay: 3,
+// 	repeat: 50});
+
+
+
+
+// const  bounceAnimaion = TweenMax.to(".ball", 2.5, { 
+	
+// 	ease: Bounce.easeOut, 
+// 	y: -50,
+// 	repeat: 50 });
+
+
+
+
+
+// const coilAnimation = TweenLite.to(".coil", 10, {
+
+// 	rotation:810, 
+// 	ease:Power3.easeInOut });
+
+
+
+// const puffAnimation = TweenLite.to(".puff", 10, {
+
+// 	rotation:810, 
+// 	ease:Power3.easeInOut });
